adfdec4d64eaf2eda942e05ade707b9b9c980e37
Gabriele Gigante <gabriele@pickeat.it>
2023-02-25 23:34:08 +0100

    url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:cloud_storage/core/files/providers/cwd_provider.dart';
 import 'package:cloud_storage/router.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'core/cache/cache.dart';
 
@@ -10,6 +11,7 @@ final ProviderContainer container = ProviderContainer();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
   final cache = container.read(cacheProvider);
   final cwd = container.read(cwdProvider);
   if (await cache.getFolder(cwd.id) == null) {
